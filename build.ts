2616febeab18ed952de95b827c5e9656b11e538c
pagoru <pagoru@gmail.com>
2024-03-19 02:43:17 +0100

    wip

--- build.ts
+++ build.ts
@@ -2,14 +2,14 @@ import { build } from 'deno_web_serve';
 import { load } from 'deno/dotenv/mod.ts';
 
 
-export const buildGame = async () => {
+export const buildGame = async (bundleAssets = true) => {
 	const env = await load();
 	Object.keys(env).forEach(key => Deno.env.set(key, env[key]));
 	
 	await build({
 		indexFileName: 'main.ts',
 		minify: true,
-		bundleAssets: true,
+		bundleAssets,
 		envs: [],
 	});
 	
